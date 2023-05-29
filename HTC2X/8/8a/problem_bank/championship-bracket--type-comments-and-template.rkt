2eb98fb1f6061ca75da103967a31e7181e4fa7db
Liam <50953884+paperclipmaximizer@users.noreply.github.com>
2023-05-29 14:18:10 +1000

    w8 championship-bracket.rtk

---
+++ HTC2X/8/8a/problem_bank/championship-bracket--type-comments-and-template.rkt
@@ -0,0 +1,37 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname championship-bracket--type-comments-and-template) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; =========================================================
+;; Data Definitions:
+
+(define-struct bracket (team-won team-lost br-won br-lost))
+;; Bracket is one of:
+;; - false
+;; - (make-bracket String String Bracket Bracket)
+;; interp.  A tournament competition bracket.
+;;    false indicates an empty bracket.
+;;    (make-bracket t1 t2 br1 br2) means that 
+;;    - team t1 beat team t2, and
+;;    - br1 represents team t1's bracket leading up to this match.
+;;    - br2 represents team t2's bracket leading up to this match.
+#;
+(define (fn-for-bracket br)
+  (cond [(false? br) (...)]
+        [else
+         (... (bracket-team-won br)
+              (bracket-team-lost br)
+              (fn-for-bracket (bracket-br-won br))
+              (fn-for-bracket (bracket-br-lost br)))]))
+
+;; ListOfTeam is one of:
+;; - empty
+;; - (cons String ListOfTeam)
+;; interp. A list of team names
+(define T0 empty)  ; no teams
+(define T2 (list "Scandal" "Traffic"))
+#;
+(define (fn-for-lot lot)
+  (cond [(empty? lot) (...)]
+        [else
+         (... (first lot)
+              (fn-for-lot (rest lot)))]))
